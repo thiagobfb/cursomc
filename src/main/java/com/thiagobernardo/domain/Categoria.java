e07c94dffcc84b30a55c768c923717b8e2e977f9
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-03-20 12:15:53 -0300

    Banco de dados H@ e criação automática da base de dados

--- src/main/java/com/thiagobernardo/domain/Categoria.java
+++ src/main/java/com/thiagobernardo/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.thiagobernardo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = -6971676764958562561L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
