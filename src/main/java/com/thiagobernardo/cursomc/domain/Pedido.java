dd53da376125dc2ea93f325172aa01d682b93d36
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-03-21 17:22:48 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/thiagobernardo/cursomc/domain/Pedido.java
+++ src/main/java/com/thiagobernardo/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.thiagobernardo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,6 +36,9 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_enterga_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Pedido() {
 	}
 
@@ -43,8 +49,6 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
-
 
 	public Integer getId() {
 		return id;
@@ -70,6 +74,14 @@ public class Pedido implements Serializable {
 		this.pagamento = pagamento;
 	}
 
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
 	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
@@ -78,12 +90,12 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
-	public Cliente getCliente() {
-		return cliente;
+	public Set<ItemPedido> getItens() {
+		return itens;
 	}
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
