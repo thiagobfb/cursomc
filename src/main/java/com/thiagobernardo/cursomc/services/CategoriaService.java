457bec615111255ca20041068ddd1da9ed7faa59
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-05-22 15:02:49 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/thiagobernardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobernardo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagobernardo.cursomc.domain.Categoria;
+import com.thiagobernardo.cursomc.dto.CategoriaDTO;
 import com.thiagobernardo.cursomc.repositories.CategoriaRepository;
 import com.thiagobernardo.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagobernardo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, 
 				Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
