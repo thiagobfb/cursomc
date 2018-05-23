d9adb5beee955e20cd2950cf40d2a4035c160ad0
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-05-23 13:03:12 -0300

    Ajustando e testando a delacao de Cliente

--- src/main/java/com/thiagobernardo/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagobernardo/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);			
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadass");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 
