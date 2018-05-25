4913534422df5022144288368046b2de1fa39544
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-05-25 17:02:52 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/thiagobernardo/cursomc/domain/Pedido.java
+++ src/main/java/com/thiagobernardo/cursomc/domain/Pedido.java
@@ -1,7 +1,6 @@
 package com.thiagobernardo.cursomc.domain;
 
 import java.io.Serializable;
-import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
