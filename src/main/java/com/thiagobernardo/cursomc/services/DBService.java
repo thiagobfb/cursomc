2efc5132deb553a40a2385de5632481e5835a4fa
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-05-26 00:21:26 -0300

    Adicionando senha a Cliente

--- src/main/java/com/thiagobernardo/cursomc/services/DBService.java
+++ src/main/java/com/thiagobernardo/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.thiagobernardo.cursomc.domain.Categoria;
@@ -34,31 +35,34 @@ import com.thiagobernardo.cursomc.repositories.ProdutoRepository;
 public class DBService {
 
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private BCryptPasswordEncoder encoder;
 	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	EstadoRepository estadoRepository;
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
-	CidadeRepository cidadeRepository;
+	private EstadoRepository estadoRepository;
 	
 	@Autowired
-	ClienteRepository clienteRepository;
+	private CidadeRepository cidadeRepository;
 	
 	@Autowired
-	EnderecoRepository enderecoRepository;
+	private ClienteRepository clienteRepository;
 	
 	@Autowired
-	PedidoRepository pedidoRepository;
+	private EnderecoRepository enderecoRepository;
 	
 	@Autowired
-	PagamentoRepository pagamentoRepository;
+	private PedidoRepository pedidoRepository;
 	
 	@Autowired
-	ItemPedidoRepository itemPedidoRepository;
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public void instantiateTestDatabase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -124,7 +128,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "thiagobf.barbosa@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "thiagobf.barbosa@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, encoder.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
